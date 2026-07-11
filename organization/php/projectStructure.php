7c3f89c56da3c159e773ab4977570780cafab188
RegySpecial <regyspecial@localhost.localdomain>
2026-07-12 01:50:20 +0200

    finally resolved issue with projectStructure.php

--- organization/php/projectStructure.php
+++ organization/php/projectStructure.php
@@ -5,17 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="project organization">
     <meta name="author" content="Regy Special">
-    <title>Document</title>
+    <title>projectStructure</title>
   </head>
   <body>
     <h1>regy-special-project</h1>
     <?php
     function searchDir(string $path){
       $entry = scandir($path);
-      for ($i=2;$i<count($entry);$i++){
-        $fileOrDir=$entry[$i];
-        $shiftColor=(["blue","lime","red"])[$i%3];
-        echo"<div style=\"background-color:$shiftColor;\">$fileOrDir</div>";
+      for ($i = 2;$i < count($entry);$i++){
+        $fileOrDir = $entry[$i];
+        $backgroundColor = (["blue","lime","red"])[$i % 3];
+        $borderColor = (["blue","lime","red"])[($i + 1) % 3];
+        echo"<div style=\"background-color:$backgroundColor;border:1px solid $borderColor;\">$fileOrDir</div>";
+        if(is_dir("$path/$fileOrDir"))searchDir("$path/$fileOrDir");
       }
     }
     searchDir("../..");
